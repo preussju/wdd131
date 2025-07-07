95f51288998f7e506bec12b6018a5b2135e9cd7f
preussju <jveiga@byupathway.edu>
2025-07-07 20:59:58 -0300

    adding js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+/* footer script */
     const Year = new Date().getFullYear();
     const YearSpan = document.getElementById('currentyear');
 
@@ -14,4 +14,16 @@ document.addEventListener('DOMContentLoaded', function () {
         lastModifiedParagraph.textContent = `Last Modified: ${lastModifiedDate}`;
     }
 
+});
+
+/* hamburguer effect script */
+
+// Store the selected elements that we are going to use. This is not required but a good practice with larger programs where the variable will be referenced more than once.
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
 });
