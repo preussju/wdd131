96ebe862d6db55a7004b2b88a4469a6939f928ff
preussju <jveiga@byupathway.edu>
2025-07-19 17:08:51 -0300

    finished

--- scripts/place.js
+++ scripts/place.js
@@ -15,3 +15,26 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 });
+
+//formula to calculate the windchill
+
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature <= 10 && windSpeed > 4.8) {  // degree celsius and km/h 
+        const windChill =
+            13.12 +
+            0.6215 * temperature -
+            11.37 * Math.pow(windSpeed, 0.16) +
+            0.3965 * temperature * Math.pow(windSpeed, 0.16);
+        return windChill.toFixed(1); // Rounded to one decimal
+    }
+    else
+    {
+        return "N/A"; // the conditions are not met, then display "N/A", which means "not applicable"
+    }
+
+}
+
+const result = calculateWindChill(22, 5);
+if (windChill) {
+    windChill.textContent = result;
+}
